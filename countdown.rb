9ee5e4764483ec3eb30108ff046f807377775657
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-03-24 13:29:26 +0300

    commit tree 11a7d0a2c9447476411c51f00c457489e100b7c8

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown count
+count = 10
+while count > 0
+  puts "#{count} SECOND(S)!"
+  count -= 1
 end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds = 5)
+while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+  sleep seconds
+end
+  return "HAPPY NEW YEAR!"
+end    
+
